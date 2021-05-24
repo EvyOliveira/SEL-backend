ed0cc3fc5bd52e8207945278b4e257df71d978ad
Evelyn Cristini Oliveira <30937883+EvyOliveira@users.noreply.github.com>
2021-05-23 21:42:47 -0300

    Refactor application package name

---
+++ src/java/edu/curso/pets/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package edu.curso.pets;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+    Usuario findByUsuario(String usuario);
+}
